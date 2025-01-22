888e8a1ba3d064652f16717014164f5d1bdd268c
kanha-13 <kanha.agr11@gmail.com>
2025-01-22 17:11:51 +0530

    added cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,21 @@
 import express, { Application } from "express";
-import bodyParser from "body-parser";
 import uploadRoutes from "./routes/upload";
 import analyzeRoutes from "./routes/analyze";
+import cors from "cors";
 
 const app: Application = express();
-app.use(bodyParser.json());
+
+const corsOptions = {
+  origin: "*",
+  // origin: "http://192.168.29.109:3000",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsOptions));
+
+// parse JSON
+app.use(express.json());
 
 app.use("/upload", uploadRoutes);
 app.use("/analyze", analyzeRoutes);
